fe845e20733de99c716a32794465df00d292b388
budzow <michal.budzowski@sonarsource.com>
2021-03-30 15:25:52 +0200

    Delete build8.bat

--- build8.bat
+++
@@ -1,2 +0,0 @@
-"D:\build-wrapper-win-x86-6.17\build-wrapper-win-x86\build-wrapper-win-x86-64.exe" --out-dir bwo msbuild.exe /t:rebuild 
-sonar-scanner -Dsonar.projectKey=Project6 -Dsonar.sources=. -Dsonar.cfamily.build-wrapper-output=bwo -Dsonar.host.url=http://l:8000 -Dsonar.login=admin -Dsonar.password=admin -X -Dsonar.login=admin -Dsonar.password=admin1
